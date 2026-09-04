116426d0ac812dee700b04f29cf7619c5aecf404
Aqua666-prog <alisakurakova397@gmail.com>
2026-09-04 17:12:37 +0300

    Migrate Room from KAPT to KSP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
-    id("org.jetbrains.kotlin.kapt")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -46,9 +46,6 @@ android {
     }
 }
 
-kapt {
-    correctErrorTypes = true
-}
 
 dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2026.06.01")
@@ -70,7 +67,7 @@ dependencies {
 
     implementation("androidx.room:room-runtime:2.8.4")
     implementation("androidx.room:room-ktx:2.8.4")
-    kapt("androidx.room:room-compiler:2.8.4")
+    ksp("androidx.room:room-compiler:2.8.4")
 
     implementation("androidx.datastore:datastore-preferences:1.2.0")
     implementation("androidx.documentfile:documentfile:1.1.0")
